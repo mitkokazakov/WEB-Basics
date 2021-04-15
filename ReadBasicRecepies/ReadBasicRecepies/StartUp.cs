af60dc975733d32083d874ed3742a0f1317dcb48
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-04-15 19:34:17 +0300

    New project added

---
+++ ReadBasicRecepies/ReadBasicRecepies/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ReadBasicRecepies
+{
+    class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            await ReadSomeText();
+        }
+
+        static async Task ReadSomeText()
+        {
+            string url = $"https://vicove.com/vic-62573";
+
+            HttpClient httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(result);
+        }
+    }
+}
