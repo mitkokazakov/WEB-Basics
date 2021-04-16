adc0166143c599de2fa6da88569ae3d31236aca1
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-04-16 20:30:34 +0300

    Implement Cookie class logic

--- SoftUniServer/SoftUniServer.HTTP/Cookie.cs
+++ SoftUniServer/SoftUniServer.HTTP/Cookie.cs
@@ -1,7 +1,21 @@
-﻿namespace SoftUniServer.HTTP
+﻿using System;
+
+namespace SoftUniServer.HTTP
 {
     public class Cookie
     {
+        public Cookie(string cookieString)
+        {
+            var cookieParts = cookieString.Split(new string[] {"; "},StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var c in cookieParts)
+            {
+                var singleCookie = c.Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+
+                this.Name = singleCookie[0];
+                this.Value = singleCookie[1];
+            }
+        }
         public string Name { get; set; }
 
         public string Value { get; set; }
