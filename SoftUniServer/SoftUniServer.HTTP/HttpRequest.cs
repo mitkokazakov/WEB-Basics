d711eeed895c8158c42daa6945a7ff99a7fde407
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-04-16 20:31:06 +0300

    Add the rest of logic for parsing headers and cookies

--- SoftUniServer/SoftUniServer.HTTP/HttpRequest.cs
+++ SoftUniServer/SoftUniServer.HTTP/HttpRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SoftUniServer.HTTP
@@ -16,10 +17,52 @@ namespace SoftUniServer.HTTP
             string firstLine = requestParts[0];
 
             var firstLineParts = firstLine.Split(new string[] { " " }, StringSplitOptions.None);
+
+            this.Method = (MethodType)Enum.Parse(typeof(MethodType), firstLineParts[0],true);
+
+            this.Path = firstLineParts[1];
+
+            bool isHeader = true;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i < requestParts.Length; i++)
+            {
+                var currentLine = requestParts[i];
+
+                if (String.IsNullOrEmpty(currentLine))
+                {
+                    isHeader = false;
+                    continue;
+                }
+
+                if (isHeader)
+                {
+                    Header header = new Header(currentLine);
+                    this.Headers.Add(header);
+
+                }
+                else 
+                {
+                    sb.Append(currentLine + "\r\n");
+                }
+            }
+
+            this.Body = sb.ToString();
+
+            Header cookieHeader = this.Headers.FirstOrDefault(h => h.Name == "Cookie");
+
+            if (cookieHeader != null)
+            {
+                Cookie cookie = new Cookie(cookieHeader.Value);
+                this.Cookies.Add(cookie);
+            }
         }
         public string Path { get; set; }
 
         public MethodType Method { get; set; }
+
+        public string Body { get; set; }
 
         public ICollection<Header> Headers { get; set; }
 
