1eb74df41597da927b02e85d4307e9fcba8a7f98
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-04-16 20:30:06 +0300

    Implement Header class logic

--- SoftUniServer/SoftUniServer.HTTP/Header.cs
+++ SoftUniServer/SoftUniServer.HTTP/Header.cs
@@ -1,7 +1,16 @@
-﻿namespace SoftUniServer.HTTP
+﻿using System;
+
+namespace SoftUniServer.HTTP
 {
     public class Header
     {
+        public Header(string headerLine)
+        {
+            var currentHeader = headerLine.Split(new string[] { ": " }, 2, StringSplitOptions.None);
+
+            this.Name = currentHeader[0];
+            this.Value = currentHeader[1];
+        }
         public string Name { get; set; }
 
         public string Value { get; set; }
