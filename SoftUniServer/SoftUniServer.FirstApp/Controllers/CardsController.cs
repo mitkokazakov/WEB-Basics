1738230c8180c546505045822213f5b6575a4da3
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-04-27 20:55:55 +0300

    Add CardsController and Implement three methods in it

---
+++ SoftUniServer/SoftUniServer.FirstApp/Controllers/CardsController.cs
@@ -0,0 +1,26 @@
+﻿using SoftUniServer.HTTP;
+using SoftUniServer.MVC;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniServer.FirstApp.Controllers
+{
+    class CardsController : Controller
+    {
+        public HttpResponse Add(HttpRequest httpRequest)
+        {
+            return this.View("Views/Cards/Add.html");
+        }
+
+        public HttpResponse All(HttpRequest httpRequest)
+        {
+            return this.View("Views/Cards/All.html");
+        }
+
+        public HttpResponse Collection(HttpRequest httpRequest)
+        {
+            return this.View("Views/Cards/Collection.html");
+        }
+    }
+}
--- SoftUniServer/SoftUniServer.FirstApp/Program.cs
+++ SoftUniServer/SoftUniServer.FirstApp/Program.cs
@@ -15,14 +15,15 @@ namespace SoftUniServer.FirstApp
             IHttpServer server = new HttpServer();
             HomeController homeController = new HomeController();
             UsersController usersController = new UsersController();
+            CardsController cardsController = new CardsController();
 
             server.AddRoute("/", homeController.Home);
             server.AddRoute("/about", homeController.About);
             server.AddRoute("/users/login", usersController.Login);
             server.AddRoute("/users/register", usersController.Register);
-            server.AddRoute("/cards/all", usersController.Register);
-            server.AddRoute("/cards/add", usersController.Register);
-            server.AddRoute("/cards/collection", usersController.Register);
+            server.AddRoute("/cards/all", cardsController.All);
+            server.AddRoute("/cards/add", cardsController.Add);
+            server.AddRoute("/cards/collection", cardsController.Collection);
 
             await server.Start(80);
 
