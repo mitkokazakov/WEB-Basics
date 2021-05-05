a1d718f52ec369d2d5dcff57e4359f629bbe57db
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-05-05 18:36:36 +0300

    Implement all database entities

---
+++ BattleCards/BattleCards/Data/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BattleCards.Data
+{
+    public class Card
+    {
+
+        public Card()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.UserCards = new HashSet<UserCard>();
+        }
+
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string Name { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public string Keyword { get; set; }
+
+        public int Attack { get; set; }
+
+        public int Health { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        public ICollection<UserCard> UserCards { get; set; }
+
+    }
+}
+
+
+
---
+++ BattleCards/BattleCards/Data/UserCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BattleCards.Data
+{
+    public class UserCard
+    {
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public string CardId { get; set; }
+
+        public virtual Card Card { get; set; }
+    }
+}
