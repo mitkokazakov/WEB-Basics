172ec3a7f1e084d3a8a6a1bd5d5aa5dafc3b1f17
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-05-05 18:36:08 +0300

    Implement all database entities

--- BattleCards/BattleCards/Data/ApplicationDbContext.cs
+++ BattleCards/BattleCards/Data/ApplicationDbContext.cs
@@ -13,6 +13,11 @@
         {
 
         }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<UserCard> UsersCards { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
