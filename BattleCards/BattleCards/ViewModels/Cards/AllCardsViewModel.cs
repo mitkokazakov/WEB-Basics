ed3d2a3d95fba54252af321aa2ffabc827d95aad
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-05-08 15:20:15 +0300

    Upload skeleton for GitExam

---
+++ BattleCards/BattleCards/ViewModels/Cards/AllCardsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleCards.ViewModels.Cards
+{
+    public class AllCardsViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Keyword { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Attack { get; set; }
+
+        public int Health { get; set; }
+
+        public string Descritpion { get; set; }
+    }
+}
