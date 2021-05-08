9991c24498a13861b4e31d306335c5da8f34c82c
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-05-08 12:30:32 +0300

    Checks in Add method added

--- BattleCards/BattleCards/Controllers/CardsController.cs
+++ BattleCards/BattleCards/Controllers/CardsController.cs
@@ -1,4 +1,5 @@
-﻿using SIS.HTTP;
+﻿using BattleCards.ViewModels.Cards;
+using SIS.HTTP;
 using SIS.MvcFramework;
 using System;
 using System.Collections.Generic;
@@ -24,9 +25,37 @@ namespace BattleCards.Controllers
         }
 
         [HttpPost]
-        public HttpResponse Add()
+        public HttpResponse Add(CardsInputModel model)
         {
-            
+            if (model.Name.Length < 5 || model.Name.Length > 15 || String.IsNullOrEmpty(model.Name))
+            {
+                return this.Error("Invalid length of the Name!");
+            }
+
+            if (String.IsNullOrEmpty(model.Image))
+            {
+                return this.Error("The ImageUrl field is required");
+            }
+
+            if (String.IsNullOrEmpty(model.Keyword))
+            {
+                return this.Error("The Keyword field is required");
+            }
+
+            if (model.Attack < 0)
+            {
+                return this.Error("The Attack can not be negative");
+            }
+
+            if (model.Health < 0)
+            {
+                return this.Error("The Health can not be negative");
+            }
+
+            if (model.Description.Length > 200 || String.IsNullOrEmpty(model.Description))
+            {
+                return this.Error("Invalid length of the description");
+            }
         }
     }
 }
