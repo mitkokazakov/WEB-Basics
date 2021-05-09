4b58f3133ad91a933386ce16fb5778b570e37080
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-05-09 18:30:02 +0300

    Create method CreateRepo in RepositoriesController

--- GitExam/Apps/Git/Controllers/RepositoriesController.cs
+++ GitExam/Apps/Git/Controllers/RepositoriesController.cs
@@ -1,4 +1,6 @@
-﻿using SUS.HTTP;
+﻿using Git.Services;
+using Git.ViewModels;
+using SUS.HTTP;
 using SUS.MvcFramework;
 using System;
 using System.Collections.Generic;
@@ -8,6 +10,13 @@ namespace Git.Controllers
 {
     public class RepositoriesController : Controller
     {
+        private readonly IRepositoryService repositoryService;
+
+        public RepositoriesController(IRepositoryService repositoryService)
+        {
+            this.repositoryService = repositoryService;
+        }
+
         public HttpResponse All()
         {
             return this.View();
@@ -16,6 +25,25 @@ namespace Git.Controllers
         public HttpResponse Create() 
         {
             return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Create(CreateRepoInputModel model)
+        {
+            string currentUserId = null;
+
+            if (this.IsUserSignedIn())
+            {
+                 currentUserId = this.GetUserId();
+            }
+
+            //TODO: Check the input !!
+
+
+            this.repositoryService.CreateRepo(model, currentUserId);
+
+            return this.Redirect("/Repositories/All");
+
         }
     }
 }
