5f8cd573d7eec371c6a873dfe15ab898eaec6aaa
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-06-20 19:46:02 +0300

    Skeleton for exam preparation has been added

---
+++ CarShopExam/CarShop/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HttpResponse Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ CarShopExam/CarShop/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace CarShop
+{
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await HttpServer
+                .WithRoutes(routes => routes
+                    .MapStaticFiles()
+                    .MapControllers())
+                .WithServices(services => services
+                    .Add<IViewEngine, CompilationViewEngine>())
+                .Start();
+    }
+}
