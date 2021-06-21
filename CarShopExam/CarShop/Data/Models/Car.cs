6965694519f70dc4bc7f2c9d3ac9be07b8c64cf3
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-06-21 18:45:08 +0300

    Add new entity Car

---
+++ CarShopExam/CarShop/Data/Models/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CarShop.Data.ModelsConstants;
+
+namespace CarShop.Data.Models
+{
+    public class Car
+    {
+        public Car()
+        {
+            this.Issues = new HashSet<Issue>();
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(ModelMaxLength)]
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        [Required]
+        public string PictureUrl { get; set; }
+
+        [Required]
+        [MaxLength(PlateNumberMaxLength)]
+        public string PlateNumber { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; }
+        public virtual User Owner { get; set; }
+
+        public virtual ICollection<Issue> Issues { get; set; }
+    }
+}
--- CarShopExam/CarShop/Data/ModelsConstants.cs
+++ CarShopExam/CarShop/Data/ModelsConstants.cs
@@ -13,5 +13,12 @@ namespace CarShop.Data
 
         public const int PasswordMaxLength = 20;
         public const int PasswordMinLength = 5;
+
+        public const int ModelMaxLength = 20;
+        public const int ModelMinLength = 5;
+
+        public const int PlateNumberMaxLength = 8;
+
+
     }
 }
