0d15229b4b92805d01417a0d826c5f7c702c5075
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-06-21 18:37:40 +0300

    Add first entity User and ModelsConstants class

---
+++ CarShopExam/CarShop/Data/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CarShop.Data.ModelsConstants;
+
+namespace CarShop.Data.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(UsernameMaxLength)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(PasswordMaxLength)]
+        public string Password { get; set; }
+
+        public bool IsMechanic { get; set; }
+    }
+}
---
+++ CarShopExam/CarShop/Data/ModelsConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Data
+{
+    public static class ModelsConstants
+    {
+        public const int UsernameMaxLength = 20;
+        public const int UsernameMinLength = 4;
+
+        public const int PasswordMaxLength = 20;
+        public const int PasswordMinLength = 5;
+    }
+}
