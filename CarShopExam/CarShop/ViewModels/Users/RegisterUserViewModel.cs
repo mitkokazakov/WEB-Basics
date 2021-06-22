c5d77cab6659aa8cc67a96e1e39536db3c5151a6
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-06-22 20:08:25 +0300

    Add Login and Register View models

---
+++ CarShopExam/CarShop/ViewModels/Users/RegisterUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CarShop.Data.ModelsConstants;
+
+namespace CarShop.ViewModels.Users
+{
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [MinLength(UsernameMinLength)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(PasswordMinLength)]
+        public string Password { get; set; }
+
+        [Required]
+        [MinLength(PasswordMinLength)]
+        public string ConfirmPassword { get; set; }
+
+        public string UserType { get; set; }
+    }
+}
