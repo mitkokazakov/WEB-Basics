dc63dd3ecf6c191fdab15ddf17e20a68fc651c1d
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-06-23 13:41:45 +0300

    Add CarsController

---
+++ CarShopExam/CarShop/Controllers/CarsController.cs
@@ -0,0 +1,47 @@
+﻿using CarShop.Services;
+using MyWebServer.Controllers;
+using MyWebServer.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly ICarsService carsService;
+
+        public CarsController(ICarsService carsService)
+        {
+            this.carsService = carsService;
+        }
+
+        public HttpResponse All()
+        {
+            if (!this.User.IsAuthenticated)
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var userId = this.User.Id;
+
+            var allCars = this.carsService.ListAllCars(userId);
+
+            return this.View(allCars);
+
+        }
+
+        
+        public HttpResponse Add()
+        {
+            if (!this.User.IsAuthenticated)
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            return this.View();
+        }
+    }
+}
