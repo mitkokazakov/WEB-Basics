ddeb8cda74cd527557b24f2f8f8587f1b52399c9
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-06-25 18:26:35 +0300

    Added two ViewModels for Issues

---
+++ CarShopExam/CarShop/ViewModels/Issues/ListAllCarsIssues.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.ViewModels.Issues
+{
+    public class ListAllCarsIssues
+    {
+        public string CarId { get; set; }
+        public string Year { get; set; }
+
+        public string Model { get; set; }
+
+        public ICollection<SingleIssueViewModel> Issues { get; set; }
+    }
+}
---
+++ CarShopExam/CarShop/ViewModels/Issues/SingleIssueViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.ViewModels.Issues
+{
+    public class SingleIssueViewModel
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+
+        public bool IsFixed { get; set; }
+    }
+}
